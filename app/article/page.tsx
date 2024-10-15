569067d1f0e1801e23819a68961aba2bdd6a0b9d
Flibouche <kevin.pfiffer2@gmail.com>
2024-10-15 16:48:43 +0200

    Add database migration and create our first page and mapping elements.

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { db } from '@/lib/db';
+import React from 'react';
 
-const page = () => {
+const ArticlePage = async () => {
+
+    // Récupérer la liste des articles
+    const articles = await db.article.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    }); 
+
     return (
         <div>
-            <h1 className='text-2xl font-bold uppercase'>Ma première page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, cumque itaque reiciendis vitae eos dolor maiores qui veritatis commodi, nesciunt optio sequi, natus recusandae? Quae eveniet totam omnis fuga accusantium.</p>
+            <h1 className='text-2xl font-bold uppercase'>Blog</h1>
+
+            {/* Afficher la liste des articles */}
+            {articles.map((article: any) => (
+                <div key={article.id} className='mb-6'>
+                    {/* Titre de l'article */}
+                    <h2 className='text-2xl font-semibold text-emerald-700'>{article.title}</h2>
+                    {/* Texte de l'article */}
+                    <p>{article.text}</p>
+                </div>
+            ))}
         </div>
     )
 }
 
-export default page
+export default ArticlePage
