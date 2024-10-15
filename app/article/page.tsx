373a22c68d206e4b4b37d8f78ae97ede218b4033
Flibouche <kevin.pfiffer2@gmail.com>
2024-10-15 15:41:19 +0200

    End of first video.

---
+++ app/article/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1 className='text-2xl font-bold uppercase'>Ma première page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, cumque itaque reiciendis vitae eos dolor maiores qui veritatis commodi, nesciunt optio sequi, natus recusandae? Quae eveniet totam omnis fuga accusantium.</p>
+        </div>
+    )
+}
+
+export default page
