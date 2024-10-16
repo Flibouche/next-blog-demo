026f108933fb10a6890ebb7eb48183a1f2448b47
Flibouche <kevin.pfiffer2@gmail.com>
2024-10-16 11:04:20 +0200

    Add eslint TailwindCSS.

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -34,30 +34,37 @@ const ArticlePage = () => {
 
     return (
         <>
-            <h1 className='text-2xl font-bold uppercase'>Blog</h1>
+            <h1 className='text-2xl font-bold uppercase m-5'>Blog</h1>
 
-            {/* Afficher la liste des articles */}
-            {articles.map((article: any) => (
-                <div key={article.id} className='mb-6'>
-                    {/* Titre de l'article */}
-                    <h2 className='text-2xl font-semibold text-emerald-700'>{article.title}</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {/* Afficher la liste des articles */}
+                {articles.map((article: any) => (
+                    <div key={article.id} className='border border-slate-500 p-6 rounded-md hover:bg-slate-700'>
+                        {/* Titre de l'article */}
+                        <h2 className='text-xl font-bold'>{article.title}</h2>
 
-                    {/* Date de création de l'article */}
-                    {article.createdAt && (
-                        <p>{formatDate(article.createdAt)}</p>
-                    )}
-                    
-                    {/* Tags de l'article */}
-                    {article.tags.map((tagArticle: any) => (
-                        <span key={tagArticle.tag.id}>
-                            {tagArticle.tag.name}
-                        </span>
-                    ))}
+                        {/* Date de création de l'article */}
+                        {article.createdAt && (
+                            <p className='text-sm text-slate-300'>{formatDate(article.createdAt)}</p>
+                        )}
 
-                    {/* Texte de l'article */}
-                    <p>{article.text}</p>
-                </div>
-            ))}
+                        <div className='flex flex-wrap gap-2 my-4'>
+                            {/* Tags de l'article */}
+                            {article.tags.map((tagArticle: any) => (
+                                <span
+                                    className='text-xs rounded-full bg-slate-600 px-3 py-2 select-none'
+                                    key={tagArticle.tag.id}
+                                >
+                                    {tagArticle.tag.name}
+                                </span>
+                            ))}
+                        </div>
+
+                        {/* Texte de l'article */}
+                        <p className='line-clamp-4'>{article.text}</p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
